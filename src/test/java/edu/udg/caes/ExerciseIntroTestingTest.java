00201eb353ff1144267de3858229b15004f6c182
Xavier Roca Nadal <xavierroca97@gmail.com>
2020-10-05 16:50:52 +0200

    Afegits nous testos.

--- src/test/java/edu/udg/caes/ExerciseIntroTestingTest.java
+++ src/test/java/edu/udg/caes/ExerciseIntroTestingTest.java
@@ -12,9 +12,23 @@ public class ExerciseIntroTestingTest {
         assertEquals(result, 0);
     }
 
+    //Test Nou
+    @Test
+    public void testFindLast2(){
+        final int result = ExerciseIntroTesting.findLast(new int []{2, 3, 5}, 3);
+        assertEquals(result, 1);
+    }
+
     @Test
     public void testLastZero(){
         final int result = ExerciseIntroTesting.lastZero(new int []{0, 1, 0});
+        assertEquals(result, 2);
+    }
+
+    //Test Nou
+    @Test
+    public void testLastZero2(){
+        final int result = ExerciseIntroTesting.lastZero(new int []{1, 1, 0});
         assertEquals(result, 2);
     }
 
@@ -24,9 +38,23 @@ public class ExerciseIntroTestingTest {
         assertEquals(result, 2);
     }
 
+    //Test Nou
+    @Test
+    public void testCountPositive2(){
+        final int result = ExerciseIntroTesting.countPositive(new int []{-4, 2, -1, 2});
+        assertEquals(result, 2);
+    }
+
     @Test
     public void testOddOrPos(){
         final int result = ExerciseIntroTesting.oddOrPos(new int []{-3, -2, 0, 1, 4});
+        assertEquals(result, 3);
+    }
+
+    //Test Nou
+    @Test
+    public void testOddOrPos2(){
+        final int result = ExerciseIntroTesting.oddOrPos(new int []{-3, -2, 2, 1, 4});
         assertEquals(result, 3);
     }
 }
